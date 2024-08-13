4f0f31343651daba360fca758208d58c0e9e20eb
Mariam22MR <xxxsaakk@gmail.com>
2024-08-13 19:52:08 +0000

    a

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
 export default function getStudentIdsSum(students) {
-  if (Array.isArray(students)) return students.reduce((prevStudent, curStud) => prevStudent + curStud.id, 0);
+  if (Array.isArray(students)) return students.reduce((prevStud, curStud) => prevStud + curStud.id, 0);
   return 0;
 }
